9b8e7fd6a6f29f1f68c92a0b96376387bf4d56a8
Jordandt22 <jordandt123411@gmail.com>
2025-05-09 04:27:58 -0700

    Changed Times for logs routes

--- server/src/controllers/logs.controller.js
+++ server/src/controllers/logs.controller.js
@@ -16,7 +16,7 @@ module.exports = {
 
     const logGroupName = "/rpc/metrics";
     const logStreamName = `rpc-${nodeName.toLowerCase()}`;
-    const metrics = await getLogs(logGroupName, logStreamName, 10, 60);
+    const metrics = await getLogs(logGroupName, logStreamName, 10, 10);
 
     res.status(200).json({
       nodeName,
@@ -34,7 +34,7 @@ module.exports = {
 
     const logGroupName = "/rpc/logs";
     const logStreamName = `rpc-${nodeName.toLowerCase()}`;
-    const logs = await getLogs(logGroupName, logStreamName, 30, 120);
+    const logs = await getLogs(logGroupName, logStreamName, 30, 30);
 
     res
       .status(200)
@@ -50,7 +50,7 @@ module.exports = {
 
     const logGroupName = "/rpc/console_logs";
     const logStreamName = `rpc-${nodeName.toLowerCase()}`;
-    const logs = await getLogs(logGroupName, logStreamName, 30, 120);
+    const logs = await getLogs(logGroupName, logStreamName, 30, 30);
 
     res
       .status(200)
